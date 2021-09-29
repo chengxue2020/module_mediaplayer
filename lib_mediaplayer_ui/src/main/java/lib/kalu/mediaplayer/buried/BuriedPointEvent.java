45477a7551c2ec6c62b2d68e6c01d915847ae285
kalu <153437803@qq.com>
2021-09-29 17:38:34 +0800

    fix code

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/buried/BuriedPointEvent.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/buried/BuriedPointEvent.java
@@ -31,60 +31,69 @@ public interface BuriedPointEvent {
 
     /**
      * 进入视频播放
-     * @param url                       视频url
+     *
+     * @param url 视频url
      */
     void playerIn(String url);
 
     /**
      * 退出视频播放
-     * @param url                       视频url
+     *
+     * @param url 视频url
      */
     void playerDestroy(String url);
 
     /**
      * 视频播放完成
-     * @param url                       视频url
+     *
+     * @param url 视频url
      */
     void playerCompletion(String url);
 
     /**
      * 视频播放异常
-     * @param url                       视频url
-     * @param isNetError                是否是网络异常
+     *
+     * @param url        视频url
+     * @param isNetError 是否是网络异常
      */
-    void onError(String url , boolean isNetError);
+    void onError(String url, boolean isNetError);
 
     /**
      * 点击了视频广告
-     * @param url                       视频url
+     *
+     * @param url 视频url
      */
     void clickAd(String url);
 
     /**
      * 视频试看点击
-     * @param url                       视频url
+     *
+     * @param url 视频url
      */
     void playerAndProved(String url);
 
 
     /**
      * 退出视频播放时候的播放进度百度分
-     * @param url                       视频url
-     * @param progress                  视频进度，计算百分比【退出时候进度 / 总进度】
+     *
+     * @param url      视频url
+     * @param progress 视频进度，计算百分比【退出时候进度 / 总进度】
      */
-    void playerOutProgress(String url , float progress);
+    void playerOutProgress(String url, float progress);
 
     /**
      * 退出视频播放时候的播放进度
-     * @param url                       视频url
-     * @param duration                  总时长
-     * @param currentPosition           当前进度时长
+     *
+     * @param url             视频url
+     * @param duration        总时长
+     * @param currentPosition 当前进度时长
      */
-    void playerOutProgress(String url, long duration , long currentPosition);
+    void playerOutProgress(String url, long duration, long currentPosition);
 
     /**
      * 视频切换音频
-     * @param url                       视频url
+     *
+     * @param url 视频url
      */
-    void videoToMedia(String url);
+    void videoToMusic(String url);
 }
