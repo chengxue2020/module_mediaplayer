234fe829ff94c93ee338f63d790f89b8ff91dd41
kalu <153437803@qq.com>
2022-04-15 10:11:06 +0800

    update api

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/config/PlayerType.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/config/PlayerType.java
@@ -1,23 +1,37 @@
 package lib.kalu.mediaplayer.config;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
 import androidx.annotation.IntDef;
 import androidx.annotation.Keep;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import tv.danmaku.ijk.media.player.IMediaPlayer;
 
+@Documented
+@Retention(CLASS)
+@Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
 @Keep
-public class PlayerType {
-
+public @interface PlayerType {
     /**
      * 播放模式
      * 普通模式，小窗口模式，正常模式三种其中一种
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface WindowType {
+    @interface WindowType {
         //普通模式
         int NORMAL = 1001;
         //全屏模式
@@ -25,21 +39,26 @@ public class PlayerType {
         //小屏模式
         int TINY = 1003;
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({NORMAL, FULL, TINY})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
     }
+
 
     /***********************/
 
     /**
      * 播放状态，主要是指播放器的各种状态
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface StateType {
+    @interface StateType {
         int STATE_INIT = 2001; // 播放未开始，即将进行
         int STATE_CLEAN = 2002; //
         int STATE_LOADING_START = 2003; // 播放准备, 开始
@@ -61,6 +80,9 @@ public class PlayerType {
 
         int STATE_SUBTITLE_START = 2017;
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({STATE_SUBTITLE_START,
                 STATE_INIT, STATE_ERROR, STATE_CLEAN,
                 STATE_START, STATE_PAUSED,
@@ -72,7 +94,6 @@ public class PlayerType {
                 STATE_ERROR_NETWORK,
                 STATE_ERROR_URL,
                 STATE_ONCE_LIVE})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
@@ -83,9 +104,11 @@ public class PlayerType {
     /**
      * 播放视频缩放类型
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface ScaleType {
+    @interface ScaleType {
         int SCREEN_SCALE_DEFAULT = 3001; //默认类型
         int SCREEN_SCALE_16_9 = 3002; //16：9比例类型，最为常见
         int SCREEN_SCALE_4_3 = 3003;  //4：3比例类型，也比较常见
@@ -93,10 +116,12 @@ public class PlayerType {
         int SCREEN_SCALE_ORIGINAL = 3005; //原始类型，指视频的原始类型
         int SCREEN_SCALE_CENTER_CROP = 3006; //剧中裁剪类型
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({SCREEN_SCALE_DEFAULT, SCREEN_SCALE_16_9,
                 SCREEN_SCALE_4_3, SCREEN_SCALE_MATCH_PARENT,
                 SCREEN_SCALE_ORIGINAL, SCREEN_SCALE_CENTER_CROP})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
@@ -107,9 +132,11 @@ public class PlayerType {
     /**
      * 播放状态
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface StatusType {
+    @interface StatusType {
         //播放完成
         int COMPLETED = 4001;
         //正在播放
@@ -119,8 +146,10 @@ public class PlayerType {
         //用户点击back。当视频退出全屏或者退出小窗口后，再次点击返回键，让用户自己处理返回键事件的逻辑
         int BACK_CLICK = 4004;
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({COMPLETED, PLAYING, PAUSE, BACK_CLICK})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
@@ -131,9 +160,11 @@ public class PlayerType {
     /**
      * 通过注解限定类型
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface RenderType {
+    @interface RenderType {
         int SURFACE = 1; // SurfaceView
         int TEXTURE = 0; // TextureView
 
@@ -149,15 +180,19 @@ public class PlayerType {
     /**
      * 通过注解限定类型
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface PlatformType {
+    @interface PlatformType {
         int ANDROID = 5001; // MediaPlayer，基于原生自带的播放器控件
         int EXO = 5002; // 基于谷歌视频播放器
         int IJK = 5003; // IjkPlayer，基于IjkPlayer封装播放器
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({IJK, ANDROID, EXO})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
@@ -166,32 +201,15 @@ public class PlayerType {
     /***********************/
 
     /**
-     * 通过注解限定类型
-     * 加载loading的类型
-     */
-    @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface LoadingType {
-        int LOADING_RING = 6001; // 是仿腾讯加载loading，其实是帧动画
-        int LOADING_QQ = 6002; // 是转圈加载loading，是补间动画
-
-        @IntDef({LOADING_RING, LOADING_QQ})
-        @Retention(RetentionPolicy.SOURCE)
-        @Keep
-        @interface Value {
-        }
-    }
-
-    /*********************/
-
-    /**
      * 控制器上的视频顶部View点击事件
      * 在竖屏模式下，默认是不显示，需要显示设置controller.setTopVisibility(true);
      * 在横屏模式下，默认是不显示，需要显示设置controller.setTvAndAudioVisibility(true,true);
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface ControllerType {
+    @interface ControllerType {
         int DOWNLOAD = 7001; // 下载
         int AUDIO = 7002; // 切换音频
         int SHARE = 7003; // 分享
@@ -199,10 +217,15 @@ public class PlayerType {
         int TV = 7005; // TV，点击投影到电视上
         int HOR_AUDIO = 7006; // 音频
 
-        @IntDef({DOWNLOAD, AUDIO,
-                SHARE, MENU, TV,
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
+        @IntDef({DOWNLOAD,
+                AUDIO,
+                SHARE,
+                MENU,
+                TV,
                 HOR_AUDIO})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
@@ -213,9 +236,11 @@ public class PlayerType {
     /**
      * 电量状态
      */
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface BatterType {
+    @interface BatterType {
         int BATTERY_CHARGING = 8001;
         int BATTERY_FULL = 8002;
         int BATTERY_10 = 8003;
@@ -224,8 +249,10 @@ public class PlayerType {
         int BATTERY_80 = 8006;
         int BATTERY_100 = 8007;
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({BATTERY_CHARGING, BATTERY_FULL, BATTERY_10, BATTERY_20, BATTERY_50, BATTERY_80, BATTERY_100})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
@@ -233,9 +260,11 @@ public class PlayerType {
 
     /********************/
 
-    @Retention(RetentionPolicy.SOURCE)
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    public @interface ErrorType {
+    @interface ErrorType {
         //错误的链接
         int ERROR_RETRY = 9001;
         //错误的链接
@@ -245,8 +274,10 @@ public class PlayerType {
         //其他异常
         int ERROR_UNEXPECTED = 9004;
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({ERROR_RETRY, ERROR_SOURCE, ERROR_PARSE, ERROR_UNEXPECTED})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
@@ -254,10 +285,11 @@ public class PlayerType {
 
     /*****************/
 
-
-    @Retention(RetentionPolicy.SOURCE)
+    @Documented
+    @Retention(CLASS)
+    @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
     @Keep
-    public @interface MediaType {
+    @interface MediaType {
         int MEDIA_INFO_URL_NULL = -1; // 视频传入url为空
         int MEDIA_INFO_VIDEO_SEEK_RENDERING_START = IMediaPlayer.MEDIA_INFO_VIDEO_SEEK_RENDERING_START; // 开始渲染视频画面
         int MEDIA_INFO_AUDIO_SEEK_RENDERING_START = IMediaPlayer.MEDIA_INFO_AUDIO_SEEK_RENDERING_START; // 开始渲染视频画面
@@ -270,8 +302,10 @@ public class PlayerType {
         int MEDIA_INFO_AUDIO_DECODED_START = IMediaPlayer.MEDIA_INFO_AUDIO_DECODED_START;
         int MEDIA_INFO_VIDEO_DECODED_START = IMediaPlayer.MEDIA_INFO_VIDEO_DECODED_START;
 
+        @Documented
+        @Retention(CLASS)
+        @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE, ANNOTATION_TYPE, PACKAGE})
         @IntDef({MEDIA_INFO_OPEN_INPUT, MEDIA_INFO_VIDEO_SEEK_RENDERING_START, MEDIA_INFO_AUDIO_SEEK_RENDERING_START, MEDIA_INFO_URL_NULL, MEDIA_INFO_AUDIO_RENDERING_START, MEDIA_INFO_VIDEO_RENDERING_START, MEDIA_INFO_BUFFERING_START, MEDIA_INFO_BUFFERING_END, MEDIA_INFO_VIDEO_ROTATION_CHANGED, MEDIA_INFO_AUDIO_DECODED_START, MEDIA_INFO_VIDEO_DECODED_START})
-        @Retention(RetentionPolicy.SOURCE)
         @Keep
         @interface Value {
         }
