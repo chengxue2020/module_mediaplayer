6f8a269ed0929c4e712201390b0470964b0015b9
kalu <153437803@qq.com>
2023-02-11 16:47:59 +0800

    update api

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/video/KernelApi.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/video/KernelApi.java
@@ -52,16 +52,7 @@ public interface KernelApi extends KernelEvent {
         boolean live = bundle.isLive();
         MPLogUtil.log("KernelApi => update => live = " + live);
         setLive(live);
-//        boolean hideStop = bundle.isHideStop();
-//        MPLogUtil.log("KernelApi => update => hideStop = " + hideStop);
-//        setHideStop(hideStop);
-//        boolean hideRelease = bundle.isHideRelease();
-//        MPLogUtil.log("KernelApi => update => hideRelease = " + hideRelease);
-//        setHideRelease(hideRelease);
-//        if (null != playUrl && playUrl.length() > 0) {
-//            setUrl(playUrl);
-//        }
-        // 2
+
         String musicUrl = bundle.getExternalMusicUrl();
         MPLogUtil.log("KernelApi => update => musicUrl = " + musicUrl);
         setExternalMusicPath(musicUrl);
