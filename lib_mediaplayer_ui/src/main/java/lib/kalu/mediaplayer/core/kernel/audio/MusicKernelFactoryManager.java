c59350849bf1aea517a115659cf58a6a4880533e
kalu <153437803@qq.com>
2023-02-14 11:35:34 +0800

    update api

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/audio/MusicKernelFactoryManager.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/kernel/audio/MusicKernelFactoryManager.java
@@ -5,6 +5,7 @@ import androidx.annotation.Keep;
 import lib.kalu.mediaplayer.config.player.PlayerType;
 import lib.kalu.mediaplayer.core.kernel.audio.android.MusicAndroidPlayerFactory;
 import lib.kalu.mediaplayer.core.kernel.audio.exo.MusicExoPlayerFactory;
+import lib.kalu.mediaplayer.core.kernel.audio.ijk.MusicIjkPlayerFactory;
 
 @Keep
 public final class MusicKernelFactoryManager {
@@ -12,6 +13,8 @@ public final class MusicKernelFactoryManager {
     public static MusicKernelFactory getFactory(@PlayerType.KernelType int type) {
         if (type == PlayerType.KernelType.EXO) {
             return MusicExoPlayerFactory.build();
+        } else if (type == PlayerType.KernelType.IJK) {
+            return MusicIjkPlayerFactory.build();
         } else {
             return MusicAndroidPlayerFactory.build();
         }
