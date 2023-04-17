ad2c01cbb2a8c85559ed63c97ed90a20a11740c0
kalu <153437803@qq.com>
2023-04-17 09:28:22 +0800

    update api

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/component/ComponentSpeed.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/core/component/ComponentSpeed.java
@@ -1,12 +1,8 @@
 package lib.kalu.mediaplayer.core.component;
 
 import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.animation.Animation;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,9 +13,6 @@ import lib.kalu.mediaplayer.config.player.PlayerType;
 import lib.kalu.mediaplayer.util.MPLogUtil;
 
 public final class ComponentSpeed extends RelativeLayout implements ComponentApi {
-
-    private Handler mHandler = null;
-    private long mUpdateTimeMillis = 0L;
 
     public ComponentSpeed(Context context) {
         super(context);
@@ -32,17 +25,11 @@ public final class ComponentSpeed extends RelativeLayout implements ComponentApi
 
     @Override
     public void onUpdateTimeMillis(@NonNull long seek, @NonNull long position, @NonNull long duration) {
-
-        if (mUpdateTimeMillis > 0L) {
-            long timeMillis = System.currentTimeMillis();
-            long value = timeMillis - mUpdateTimeMillis;
-            MPLogUtil.log("ComponentSpeed => onUpdateTimeMillis => value = " + value);
-            if (value <= 1000) {
-                return;
-            } else {
-                mUpdateTimeMillis = timeMillis;
-            }
-        }
+        boolean showing = isShowing();
+        MPLogUtil.log("ComponentSpeed => onUpdateTimeMillis => showing = " + showing);
+        if (!showing)
+            return;
+        updateSpeed();
     }
 
     @Override
@@ -50,50 +37,52 @@ public final class ComponentSpeed extends RelativeLayout implements ComponentApi
         switch (playState) {
             case PlayerType.StateType.STATE_BUFFERING_START:
                 MPLogUtil.log("ComponentSpeed => onPlayStateChanged => playState = " + playState);
-                bringToFront();
-                findViewById(R.id.module_mediaplayer_component_speed_bg).setVisibility(View.VISIBLE);
-                findViewById(R.id.module_mediaplayer_component_speed_pb).setVisibility(View.VISIBLE);
-                findViewById(R.id.module_mediaplayer_component_speed_txt).setVisibility(View.VISIBLE);
-                findViewById(R.id.module_mediaplayer_component_speed_unit).setVisibility(View.VISIBLE);
-                if (null == mHandler) {
-                    mHandler = new Handler(Looper.myLooper()) {
-                        @Override
-                        public void handleMessage(@NonNull Message msg) {
-                            super.handleMessage(msg);
-                            if (null != msg && msg.what == 7677) {
-                                try {
-                                    String speed = getPlayerApi().getTcpSpeed();
-                                    int length = speed.length();
-                                    int start = length - 4;
-                                    String unit = speed.substring(start, length);
-                                    String num = speed.substring(0, start);
-                                    MPLogUtil.log("ComponentSpeed => updateSpeed => speed = " + speed);
-                                    TextView v1 = findViewById(R.id.module_mediaplayer_component_speed_txt);
-                                    v1.setText(num);
-                                    TextView v2 = findViewById(R.id.module_mediaplayer_component_speed_unit);
-                                    v2.setText(unit);
-                                } catch (Exception e) {
-                                }
-                            }
-                            mHandler.sendEmptyMessageDelayed(7677, 1000);
-                        }
-                    };
-                    mHandler.sendEmptyMessage(7677);
-                }
+                startSpeed();
                 break;
             case PlayerType.StateType.STATE_INIT:
             case PlayerType.StateType.STATE_LOADING_START:
             case PlayerType.StateType.STATE_BUFFERING_STOP:
                 MPLogUtil.log("ComponentSpeed => onPlayStateChanged => playState = " + playState);
-                findViewById(R.id.module_mediaplayer_component_speed_bg).setVisibility(View.GONE);
-                findViewById(R.id.module_mediaplayer_component_speed_pb).setVisibility(View.GONE);
-                findViewById(R.id.module_mediaplayer_component_speed_txt).setVisibility(View.GONE);
-                findViewById(R.id.module_mediaplayer_component_speed_unit).setVisibility(View.GONE);
-                if (null != mHandler) {
-                    mHandler.removeMessages(7677);
-                    mHandler = null;
-                }
+                cleanSpeed();
                 break;
+        }
+    }
+
+    private void updateSpeed() {
+        try {
+            String speed = getPlayerApi().getTcpSpeed();
+            int length = speed.length();
+            int start = length - 4;
+            String unit = speed.substring(start, length);
+            String num = speed.substring(0, start);
+            TextView v1 = findViewById(R.id.module_mediaplayer_component_speed_txt);
+            v1.setText(num);
+            TextView v2 = findViewById(R.id.module_mediaplayer_component_speed_unit);
+            v2.setText(unit);
+        } catch (Exception e) {
+        }
+    }
+
+    private boolean isShowing() {
+        try {
+            return findViewById(R.id.module_mediaplayer_component_speed).getVisibility() == View.VISIBLE;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private void cleanSpeed() {
+        try {
+            findViewById(R.id.module_mediaplayer_component_speed).setVisibility(View.GONE);
+        } catch (Exception e) {
+        }
+    }
+
+    private void startSpeed() {
+        try {
+            bringToFront();
+            findViewById(R.id.module_mediaplayer_component_speed).setVisibility(View.VISIBLE);
+        } catch (Exception e) {
         }
     }
 }
