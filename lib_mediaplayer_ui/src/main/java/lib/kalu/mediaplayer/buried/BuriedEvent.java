d244c69ee32fddae37b750219ddbbc84d4e32f6f
kalu <153437803@qq.com>
2023-04-18 11:40:07 +0800

    update api

--- lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/buried/BuriedEvent.java
+++ lib_mediaplayer_ui/src/main/java/lib/kalu/mediaplayer/buried/BuriedEvent.java
@@ -6,43 +6,23 @@ import androidx.annotation.NonNull;
 @Keep
 public interface BuriedEvent {
 
-    void onPlaying(@NonNull String url,
-                   @NonNull long position,
-                   @NonNull long duration);
+    void onPlaying(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onExit(@NonNull String url,
-                @NonNull long position,
-                @NonNull long duration);
+    void onExit(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onCompletion(@NonNull String url,
-                      @NonNull long position,
-                      @NonNull long duration);
+    void onCompletion(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onError(@NonNull String url,
-                 @NonNull long position,
-                 @NonNull long duration);
+    void onError(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onPause(@NonNull String url,
-                 @NonNull long position,
-                 @NonNull long duration);
+    void onPause(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onResume(@NonNull String url,
-                  @NonNull long position,
-                  @NonNull long duration);
+    void onResume(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onSeek(@NonNull String url,
-                @NonNull long position,
-                @NonNull long duration);
+    void onSeek(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onWindowFull(@NonNull String url,
-                      @NonNull long position,
-                      @NonNull long duration);
+    void onWindowFull(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onWindowFloat(@NonNull String url,
-                       @NonNull long position,
-                       @NonNull long duration);
+    void onWindowFloat(@NonNull String url, @NonNull long position, @NonNull long duration);
 
-    void onWindowSimple(@NonNull String url,
-                        @NonNull long position,
-                        @NonNull long duration);
+    void onWindowSimple(@NonNull String url, @NonNull long position, @NonNull long duration);
 }
